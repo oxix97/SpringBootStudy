b3349f1905286a3189bed84c9f5c9410fedc7c8e
username <ieejo716@naver.com>
2023-02-09 11:02:49 +0900

    JsonInclude를 통해 null 값을 허용하지 않을 수 있다. reponse에 null이 포함된 변수는 return 하지 않는 것을 확인할 수 있다.

--- RegularResponseExample/test/src/main/java/com/example/test/dto/User.java
+++ RegularResponseExample/test/src/main/java/com/example/test/dto/User.java
@@ -1,9 +1,11 @@
 package com.example.test.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class User {
     private String name;
     private int age;

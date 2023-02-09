27dd1ec28b005b1d64ef136c1cc7010a0f31d527
username <ieejo716@naver.com>
2023-02-09 11:30:29 +0900

    object mapper 사용시 주의할 점

--- ObjectMapperExample/demo/src/main/java/com/example/demo/dto/User.java
+++ ObjectMapperExample/demo/src/main/java/com/example/demo/dto/User.java
@@ -7,6 +7,14 @@ public class User {
     public User() {
     }
 
+
+    // obejct mapper를 이용하는 경우 getter를 사용하기 때문에 메서드 첫 네임 으로  get를 붙이면 안된다.
+    // 실행하게 되면 get -> get -> ... 무한 반복이 일어나며 StackOverflowError가 나오게 된다.
+
+//    public User getUser() {
+//        return new User("user", 1);
+//    }
+
     public User(String name, int age) {
         this.name = name;
         this.age = age;

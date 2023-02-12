db63b93d2a0f64e25a7b5607f5c904e341157cb0
username <ieejo716@naver.com>
2023-02-12 18:29:04 +0900

    @Valid 를 사용하여 검증해야하는 변수라는 것을 알림

--- Validation/SpringBootValidation/src/main/java/com/example/springbootvalidation/controller/ApiController.java
+++ Validation/SpringBootValidation/src/main/java/com/example/springbootvalidation/controller/ApiController.java
@@ -1,8 +1,7 @@
 package com.example.springbootvalidation.controller;
 
 import com.example.springbootvalidation.dto.User;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ApiController {
 
-
     @PostMapping("/user")
-    public Object user(@RequestBody User user) {
-
-        if (user.getAge() > 100) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(user);
-        }
-
-        System.out.println(user);
+    public Object user(@Valid @RequestBody User user) {
         return user;
     }
 }

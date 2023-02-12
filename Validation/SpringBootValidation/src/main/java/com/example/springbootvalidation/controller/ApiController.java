0f7d798dc6a50bd763ca15e944e87c33adc526f3
username <ieejo716@naver.com>
2023-02-12 18:21:39 +0900

    Spring Boot 기능 미사용 Validation

--- Validation/SpringBootValidation/src/main/java/com/example/springbootvalidation/controller/ApiController.java
+++ Validation/SpringBootValidation/src/main/java/com/example/springbootvalidation/controller/ApiController.java
@@ -1,6 +1,8 @@
 package com.example.springbootvalidation.controller;
 
 import com.example.springbootvalidation.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,11 @@ public class ApiController {
 
     @PostMapping("/user")
     public Object user(@RequestBody User user) {
+
+        if (user.getAge() > 100) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(user);
+        }
+
         System.out.println(user);
         return user;
     }

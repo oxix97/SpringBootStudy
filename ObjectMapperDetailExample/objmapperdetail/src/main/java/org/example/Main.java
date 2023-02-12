b4a8aab540dcac8fcd08749ce78dfdee5516d4fc
username <ieejo716@naver.com>
2023-02-12 16:39:26 +0900

    basic object mapper example

--- ObjectMapperDetailExample/objmapperdetail/src/main/java/org/example/Main.java
+++ ObjectMapperDetailExample/objmapperdetail/src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.dto.Car;
+import org.example.dto.User;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        User user = new User();
+        user.setName("Chan");
+        user.setAge(27);
+
+
+        Car car1 = new Car();
+        car1.setName("K3");
+        car1.setcarNumber("11나 1111");
+        car1.setType("sedan");
+
+        Car car2 = new Car();
+        car2.setName("Q5");
+        car2.setcarNumber("22나 2222");
+        car2.setType("SUV");
+
+        List<Car> carList = Arrays.asList(car1, car2);
+        user.setCars(carList);
+
+        String json = objectMapper.writeValueAsString(user);
+        System.out.println(json);
     }
 }

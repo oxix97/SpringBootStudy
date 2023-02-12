9bcbc86895b79f66dc7a3a584b6b0fecde646ec2
username <ieejo716@naver.com>
2023-02-12 16:51:09 +0900

    JsonNode를 사용하여 데이터를 선택 후 매핑, ObjectNode를 사용하여 데이터 변경

--- ObjectMapperDetailExample/objmapperdetail/src/main/java/org/example/Main.java
+++ ObjectMapperDetailExample/objmapperdetail/src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.example.dto.Car;
 import org.example.dto.User;
 
@@ -32,5 +36,24 @@ public class Main {
 
         String json = objectMapper.writeValueAsString(user);
         System.out.println(json);
+
+        JsonNode jsonNode = objectMapper.readTree(json);
+        String _name = jsonNode.get("name").asText();
+        int _age = jsonNode.get("age").asInt();
+        System.out.println("-------------");
+        System.out.println("name : " + _name);
+        System.out.println("age : " + _age);
+
+        JsonNode cars = jsonNode.get("cars");
+        ArrayNode arrayNode = (ArrayNode) cars;
+
+        List<Car> _cars = objectMapper.convertValue(arrayNode, new TypeReference<List<Car>>() {
+        });
+        System.out.println(_cars);
+
+        ObjectNode objectNode = (ObjectNode) jsonNode;
+        objectNode.put("name", "KIM");
+
+        System.out.println(objectNode.toPrettyString());
     }
 }

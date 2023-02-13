9d40d1d678f0fa22daa21354f39e53d51281bcc8
username <ieejo716@naver.com>
2023-02-13 16:22:56 +0900

    init setting

---
+++ Validation/BestPracticeValidation/src/main/java/com/example/bestpracticevalidation/BestPracticeValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.bestpracticevalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestPracticeValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestPracticeValidationApplication.class, args);
+    }
+
+}

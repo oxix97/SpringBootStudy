c67ee86c6f1e2682b419f0a0f62d84f9a63a5c9e
username <ieejo716@naver.com>
2023-02-14 21:37:18 +0900

    init setting

---
+++ Interceptor/InterceptorExample/src/main/java/com/example/interceptorexample/InterceptorExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.interceptorexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterceptorExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterceptorExampleApplication.class, args);
+    }
+
+}

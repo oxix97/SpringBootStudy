13917f5aaf95f7337c4b7c03a68cb8c143fca0cd
username <ieejo716@naver.com>
2023-02-14 12:40:14 +0900

    Lombok example

--- SpringFilter/Filter/src/main/java/com/example/filter/dto/User.java
+++ SpringFilter/Filter/src/main/java/com/example/filter/dto/User.java
@@ -1,16 +1,13 @@
 package com.example.filter.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter //get method
-@Setter // set method
+//@Getter //get method
+//@Setter // set method
+@Data //getter + setter + toString
 @NoArgsConstructor //default constructor
 @AllArgsConstructor // all constructor
 public class User {
-
     private String name;
     private int age;
 }

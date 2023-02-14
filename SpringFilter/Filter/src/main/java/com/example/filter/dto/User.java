3ee9ca32d8155545aa77257c5945f65766a24fc3
username <ieejo716@naver.com>
2023-02-14 15:43:23 +0900

    Lombok example

--- SpringFilter/Filter/src/main/java/com/example/filter/dto/User.java
+++ SpringFilter/Filter/src/main/java/com/example/filter/dto/User.java
@@ -2,12 +2,13 @@ package com.example.filter.dto;
 
 import lombok.*;
 
-//@Getter //get method
-//@Setter // set method
+
 @Data //getter + setter + toString
 @NoArgsConstructor //default constructor
 @AllArgsConstructor // all constructor
+
 public class User {
     private String name;
+    private String email;
     private int age;
 }

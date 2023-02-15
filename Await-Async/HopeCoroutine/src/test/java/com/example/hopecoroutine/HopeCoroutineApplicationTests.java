885010579a7ec07a3d1f2a6c938eeff6c8adccd3
username <ieejo716@naver.com>
2023-02-15 12:16:58 +0900

    init setting

---
+++ Await-Async/HopeCoroutine/src/test/java/com/example/hopecoroutine/HopeCoroutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hopecoroutine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopeCoroutineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

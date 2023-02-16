0987dc46941bf544efd8b1bedf3a8ebed6222ff8
username <ieejo716@naver.com>
2023-02-16 23:28:04 +0900

    init setting

---
+++ SimpleNaverApiUse/naver-api-example/src/main/java/com/example/naverapiexample/NaverApiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.naverapiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverApiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaverApiExampleApplication.class, args);
+    }
+
+}

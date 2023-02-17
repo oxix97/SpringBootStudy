a8a41de492a2754dedf745cbd3c633ff2d3d3c39
username <ieejo716@naver.com>
2023-02-17 16:43:00 +0900

    이전에 구현했던 환율 계산

---
+++ JUnit-Exmaple/UnitTest/SpringJUnit/src/main/java/com/example/springjunit/calculator/DollarCalculator.java
@@ -0,0 +1,25 @@
+package com.example.springjunit.calculator;
+
+import org.example.api.MarketApi;
+
+public class DollarCalculator implements ICalculator {
+    private int price = 1;
+
+    public DollarCalculator(MarketApi api) {
+        this.price = api.connect();
+    }
+
+    @Override
+    public int sum(int x, int y) {
+        x *= this.price;
+        y *= this.price;
+        return x + y;
+    }
+
+    @Override
+    public int minus(int x, int y) {
+        x *= this.price;
+        y *= this.price;
+        return x - y;
+    }
+}
---
+++ JUnit-Exmaple/UnitTest/SpringJUnit/src/main/java/com/example/springjunit/calculator/ICalculator.java
@@ -0,0 +1,6 @@
+package com.example.springjunit.calculator;
+
+public interface ICalculator {
+    public int sum(int x,int y);
+    public int minus(int x,int y);
+}

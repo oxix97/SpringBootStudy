cf428757207ccdf64b46167ad7c375d2931523db
username <ieejo716@naver.com>
2023-02-18 21:50:56 +0900

    Naver 지역 검색 구현

---
+++ SimpleNaverApiUse/NaverSearchAPI/searchlist/src/main/java/com/example/searchlist/service/NaverService.java
@@ -0,0 +1,42 @@
+package com.example.searchlist.service;
+
+import com.example.searchlist.dto.ReqLocalSearch;
+import com.example.searchlist.dto.ResLocalSearch;
+import com.example.searchlist.service.base.BaseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+@RequiredArgsConstructor
+@Service
+public class NaverService {
+    private final BaseService service;
+
+    @Value("${url_local}")
+    private String localUrl;
+    @Value("${url_image}")
+    private String imageUrl;
+
+    public ResLocalSearch searchLocal(ReqLocalSearch query) {
+        BaseService base = new BaseService();
+        URI uri = service.uriBuilder(query.toMultiValueMap(), localUrl);
+        HttpHeaders headers = service.baseHeaders();
+
+        var httpEntity = new HttpEntity<>(headers);
+        var responseType = new ParameterizedTypeReference<ResLocalSearch>() {
+        };
+
+        var response = new RestTemplate().exchange(uri, HttpMethod.GET, httpEntity, responseType);
+
+        return response.getBody();
+    }
+
+    public void searchImage() {
+
+    }
+}

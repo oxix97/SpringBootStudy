5403e38cc2dfddf21d791ed95131eff8313611ef
username <ieejo716@naver.com>
2023-02-18 23:24:57 +0900

    int index -> Integer index

--- SimpleNaverApiUse/NaverSearchAPI/searchlist/src/main/java/com/example/searchlist/dto/WishListDto.java
+++ SimpleNaverApiUse/NaverSearchAPI/searchlist/src/main/java/com/example/searchlist/dto/WishListDto.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 
 @Data
 public class WishListDto {
-    private int index;
+    private Integer index;
     private String title;
     private String category;
     private String address;

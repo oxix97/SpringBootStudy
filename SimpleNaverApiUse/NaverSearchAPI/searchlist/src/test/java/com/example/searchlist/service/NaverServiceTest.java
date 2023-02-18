386c282df3a51831ad1172b6fcc9a6c6cd6a8af2
username <ieejo716@naver.com>
2023-02-18 21:51:22 +0900

    Naver 지역 검색 호출 테스트

---
+++ SimpleNaverApiUse/NaverSearchAPI/searchlist/src/test/java/com/example/searchlist/service/NaverServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.searchlist.service;
+
+import com.example.searchlist.dto.ReqLocalSearch;
+import com.example.searchlist.service.base.BaseService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+public class NaverServiceTest {
+
+    @Autowired
+    private NaverService service;
+
+    @Test
+    public void localSearchTest() {
+        var search = new ReqLocalSearch();
+        search.setQuery("갈비집");
+
+        var result = service.searchLocal(search);
+        System.out.println(result);
+    }
+}

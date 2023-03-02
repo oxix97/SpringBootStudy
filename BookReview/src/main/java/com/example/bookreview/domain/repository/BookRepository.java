b4f4bbd62b43f755fb73369bce586719c1557e00
username <ieejo716@naver.com>
2023-03-02 23:18:11 +0900

    @Query 사용

--- BookReview/src/main/java/com/example/bookreview/domain/repository/BookRepository.java
+++ BookReview/src/main/java/com/example/bookreview/domain/repository/BookRepository.java
@@ -2,9 +2,15 @@ package com.example.bookreview.domain.repository;
 
 import com.example.bookreview.domain.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+//    Book findByCategoryIsNullAndNameEqualsAndCreatedAtGreaterThanEuqualAndUpdatedAtGreaterThanEqual(String name);
 
+    @Query(value = "select b from Book b where name = :name and createdAt >= :createdAt and updatedAt >= :updatedAt and category is null")
+    List<Book> findByRecently(@Param("name") String name);
 }

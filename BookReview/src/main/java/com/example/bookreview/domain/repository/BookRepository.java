d58067424d29bd5846a3efc87aa7e884c8876e7c
username <ieejo716@naver.com>
2023-03-02 23:31:17 +0900

    @Query, dto를 사용하여 데이터 받아오기

--- BookReview/src/main/java/com/example/bookreview/domain/repository/BookRepository.java
+++ BookReview/src/main/java/com/example/bookreview/domain/repository/BookRepository.java
@@ -1,6 +1,8 @@
 package com.example.bookreview.domain.repository;
 
 import com.example.bookreview.domain.entity.Book;
+import com.example.bookreview.domain.repository.dto.BookNameAndCategory;
+import jakarta.persistence.Tuple;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,4 +15,7 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Query(value = "select b from Book b where name = :name and createdAt >= :createdAt and updatedAt >= :updatedAt and category is null")
     List<Book> findByRecently(@Param("name") String name);
+
+    @Query(value = "select b.name as name, b.category as category from Book  b")
+    List<Tuple> findBookNameAndCategory();
 }

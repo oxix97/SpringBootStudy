dc97f4b4b34d723aa216d7d55cb136a0ece446c5
username <ieejo716@naver.com>
2023-03-09 15:32:49 +0900

    init setting

---
+++ BookStoreTest/src/main/java/com/example/bookstoretest/BookStoreTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookstoretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreTestApplication.class, args);
+    }
+
+}

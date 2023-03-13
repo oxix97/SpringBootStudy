1330accf6a3baa381799366eb7afab5c9cc82589
username <ieejo716@naver.com>
2023-03-13 22:50:10 +0900

    Security Config

--- SecurityTest/src/main/java/com/example/securitytest/web/config/SecurityConfig.java
+++ SecurityTest/src/main/java/com/example/securitytest/web/config/SecurityConfig.java
@@ -1,17 +1,15 @@
 package com.example.securitytest.web.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebSecurity(debug = true)
-@EnableMethodSecurity
 @Configuration
-public class SecurityConfig {
-    @Bean
-    public WebSecurityConfiguration webSecurityCustomizer() {
-
+public class SecurityConfig implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
     }
 }

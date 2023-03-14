022cc68351039ae8dc9cd0843ab21ccbd2885e77
username <ieejo716@naver.com>
2023-03-14 18:00:54 +0900

    init setting

---
+++ ServletExample/src/main/java/com/example/servletexample/ServletExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.servletexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServletExampleApplication.class, args);
+    }
+
+}
---
+++ ServletExample/src/test/java/com/example/servletexample/ServletExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servletexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

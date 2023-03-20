5f665aae398cea4683dde398317029647fa23f0a
username <ieejo716@naver.com>
2023-03-20 21:45:11 +0900

    [FIX] servlet exam

--- ServletExample/src/main/java/com/example/servletexample/ServletExampleApplication.java
+++ ServletExample/src/main/java/com/example/servletexample/ServletExampleApplication.java
@@ -2,8 +2,11 @@ package com.example.servletexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class ServletExampleApplication {
 
     public static void main(String[] args) {

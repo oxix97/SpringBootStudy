268970a53c34f213ad3645fd11e7d9efac18070b
username <ieejo716@naver.com>
2023-04-11 16:59:05 +0900

    init setting

---
+++ OOP-Calculator/WebCalculator/src/main/java/calculator/OOPCalculator.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import java.util.List;
+
+public class OOPCalculator {
+    private static final List<NewArithmeticOperator> operators = List.of(
+            new Addition(),
+            new Subtraction(),
+            new Multiplication(),
+            new Division()
+    );
+    public static int calculate(int operand1, int operand2, String operator) {
+        return operators.stream()
+                .filter(it -> it.supports(operator))
+                .map(it -> it.calculate(operand1,operand2))
+                .findFirst()
+                .orElseThrow(()->new IllegalArgumentException("올바른 연산자가 아닙니다."));
+    }
+}

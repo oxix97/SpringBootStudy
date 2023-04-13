79e3cb2f4743ed317dda7e7e91a9e5b4ab4aa4a3
username <ieejo716@naver.com>
2023-04-13 16:10:30 +0900

    [CHORE] : 임시커밋

---
+++ OOP-Calculator/WebCalculator/src/test/java/server/QueryStringTest.java
@@ -0,0 +1,10 @@
+package server;
+
+import org.junit.jupiter.api.Test;
+
+public class QueryStringTest {
+    @Test
+    void createTest() {
+//        QueryString qs1 = new QueryString("operand1",11);
+    }
+}
---
+++ OOP-Calculator/WebCalculator/src/test/java/server/QueryStringsTest.java
@@ -0,0 +1,10 @@
+package server;
+
+import org.junit.jupiter.api.Test;
+
+public class QueryStringsTest {
+    @Test
+    void createdTest(){
+        QueryStrings queryStrings = new QueryStrings("");
+    }
+}

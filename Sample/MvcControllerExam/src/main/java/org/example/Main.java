7d705d79230238f49c3fd616beb6862bd7e2c034
username <ieejo716@naver.com>
2023-04-17 19:21:17 +0900

    init setting

---
+++ Sample/MvcControllerExam/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) throws LifecycleException {
+        String location = "webapps/";
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        tomcat.addWebapp("/", new File(location).getAbsolutePath());
+        log.info("configuring app with base dir : {}", new File("./" + location).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}

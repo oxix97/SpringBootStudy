afc5d4ff2bab78989d9e7f99a2938a9ec804d6fd
username <ieejo716@naver.com>
2023-04-17 16:58:26 +0900

    User class에 대한 정보를 가져오는 테스트

--- Sample/MVC-ControllerExample/ControllerSample/src/test/java/ReflectionTest.java
+++ Sample/MVC-ControllerExample/ControllerSample/src/test/java/ReflectionTest.java
@@ -1,12 +1,18 @@
 import example.annotation.Controller;
 import example.annotation.Service;
+import example.model.User;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 //todo Controller 어노테이션이 설정되어있는 모든 클래스를 찾아 출력을 목적
 
@@ -15,12 +21,26 @@ public class ReflectionTest {
 
     @Test
     void controllerScan() {
+        Set<Class<?>> beans = getTypesAnnotatedWith(List.of(Controller.class, Service.class));
+        log.info("beans : [{}]", beans);
+    }
+
+    private Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
         Reflections reflection = new Reflections("example");
 
         //해당 org.exmaple 밑에 있는 코드중에 Controller.class가 붙어있는 클래스들을 beans에다가 담는다.
         Set<Class<?>> beans = new HashSet<>();
-        beans.addAll(reflection.getTypesAnnotatedWith(Controller.class));
-        beans.addAll(reflection.getTypesAnnotatedWith(Service.class));
-        log.info("beans : [{}]", beans);
+        annotations.forEach(it -> beans.addAll(reflection.getTypesAnnotatedWith(it)));
+        return beans;
+    }
+
+    @Test
+    void showClass() {
+        Class<User> clazz = User.class;
+        log.debug(clazz.getName());
+
+        log.info("User all declared fields : [{}]", Arrays.stream(clazz.getDeclaredFields()).collect(Collectors.toList()));
+        log.info("User all declared constructor : [{}]", Arrays.stream(clazz.getDeclaredConstructors()).collect(Collectors.toList()));
+        log.info("User all declared method : [{}]", Arrays.stream(clazz.getDeclaredMethods()).collect(Collectors.toList()));
     }
 }

46ff6f765e84f9a92b4edfc7499bdf6717a58cb4
username <ieejo716@naver.com>
2023-04-17 15:08:22 +0900

    OAuth example delete

--- OAuthTest/src/main/java/com/example/oauthtest/repository/PostsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.oauthtest.repository;
-
-import com.example.oauthtest.domain.posts.Posts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostsRepository extends JpaRepository<Posts, Long> {
-
-}

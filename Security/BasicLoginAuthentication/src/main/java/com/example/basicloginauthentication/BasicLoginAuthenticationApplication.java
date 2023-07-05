775c72d91949e0adf19d7c6d849120f5801ab993
Chan <ieejo716@naver.com>
2023-07-05 18:41:44 +0900

    SpringApplication

---
+++ Security/BasicLoginAuthentication/src/main/java/com/example/basicloginauthentication/BasicLoginAuthenticationApplication.java
@@ -0,0 +1,14 @@
+package com.example.basicloginauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+public class BasicLoginAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicLoginAuthenticationApplication.class, args);
+    }
+
+}

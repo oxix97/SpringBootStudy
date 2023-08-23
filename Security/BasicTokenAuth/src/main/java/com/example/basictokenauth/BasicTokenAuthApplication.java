d847fe570b88c70f82d744baca8788c462516438
Chan <ieejo716@naver.com>
2023-08-23 17:18:28 +0900

    init setting

---
+++ Security/BasicTokenAuth/src/main/java/com/example/basictokenauth/BasicTokenAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.basictokenauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicTokenAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicTokenAuthApplication.class, args);
+    }
+
+}
---
+++ Security/BasicTokenAuth/src/test/java/com/example/basictokenauth/BasicTokenAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basictokenauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicTokenAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

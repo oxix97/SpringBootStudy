3f2a41b655628bb414a727b58e6cbd744f86157c
Chan <ieejo716@naver.com>
2023-12-22 12:32:12 +0900

    Settings : 세팅

---
+++ Batch/BasicBatch/src/main/java/org/example/BasicBatchApplication.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+@EnableBatchProcessing
+@SpringBootApplication
+public class BasicBatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BasicBatchApplication.class, args);
+    }
+}
